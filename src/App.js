d743ed8b55d323a1b19bfc962e133e55d3d29da3
Yevhen <balagutrak.jenia@gmail.com>
2022-05-25 22:58:42 +0200

    Fade in effect added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import FadeInSection from './FadeInSection/FadeInsSection';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <header className="App-header">
+        <FadeInSection>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+      </FadeInSection>
       </header>
     </div>
   );
